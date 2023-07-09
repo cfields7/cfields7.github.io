0bcae3d00a6088b7d2a3c28e623bcf2bc0b71044
cfields7 <christopher.fields@me.com>
2023-07-09 14:13:05 -0400

    nav test

--- resources/js/common.js
+++ resources/js/common.js
@@ -6,3 +6,30 @@ function openHamburger() {
     x.className = "topnav";
   }
 }
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.histoy.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const routes = {
+  404: "index.html",
+  "/": "index.html",
+  "/projects": "projects.html",
+  "/about": "about.html",
+  "/music": "music.html"
+}
+
+const handleLocation = async() => {
+  const pathname = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html  = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+}
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
